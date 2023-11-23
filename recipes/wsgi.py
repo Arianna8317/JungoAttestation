58bdfade01846521c5acd232f3cd7ad41c7cf394
Arianna8317 <stanlyfiji@gmail.com>
2023-11-23 21:55:12 +0300

    last

--- recipes/wsgi.py
+++ recipes/wsgi.py
@@ -8,9 +8,18 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 """
 
 import os
+import sys
+from dotenv import load_dotenv
+project_folder = os.path.expanduser('~/recipes') # adjust as
+
+load_dotenv(os.path.join(project_folder, '.env'))
+# assuming your django settings file is at '/home/username/mysite/mysite/settings.py'
+#  and your manage.py is is at '/home/username/mysite/manage.py'
+path = '/home/Irafita/recipes'
+if path not in sys.path:
+    sys.path.append(path)
+
+os.environ['DJANGO_SETTINGS_MODULE'] = 'recipes.settings'
 
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'recipes.settings')
-
 application = get_wsgi_application()
