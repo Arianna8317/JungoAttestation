6c9818c981ce11efb9b91ff7fea96c141d9c5e9b
Arianna8317 <stanlyfiji@gmail.com>
2023-11-23 22:02:40 +0300

    last

--- recipes/settings.py
+++ recipes/settings.py
@@ -20,16 +20,23 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ldg!l*a^hajt%ri7jd^wvnz3apt!9#alg=&=#m3pn(i#st#gd^'
+#SECRET_KEY = 'django-insecure-ldg!l*a^hajt%ri7jd^wvnz3apt!9#alg=&=#m3pn(i#st#gd^'
 
+import os
+SECRET_KEY = os.getenv('SECRET_KEY')
+#SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&k3q+pmu)5%asj6yjpkag')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
 ALLOWED_HOSTS = [
     '127.0.0.1:8000',
-    '127.0.0.1'
+    '127.0.0.1',
+    'Irafita.pythonanywhere.com',
+
 ]
 
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
 
 # Application definition
 
@@ -42,9 +49,9 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'mainapp',
     'users',
-    'crispy_forms',
+
 ]
-CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
@@ -82,8 +89,15 @@ WSGI_APPLICATION = 'recipes.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    'ENGINE': 'django.db.backends.mysql',
+    'NAME': 'Irafita$recipes',
+    'USER': 'Irafita',
+    'PASSWORD': os.getenv('MYSQL_PASSWORD'),
+    'HOST': 'Irafita.mysql.pythonanywhere-services.com',
+    'OPTIONS':{
+        'init_command': "SET NAMES 'utf8mb4'; SET sql_mode='STRICT_TRANS_TABLES'",
+        'charset': 'utf8mb4',
+                },
     }
 }
 
@@ -128,3 +142,12 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.10/howto/static-files/
+
+# The absolute path to the directory where collectstatic will collect static files for deployment.
+#STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = BASE_DIR / 'static/'
+
